87d0684ed15dc63af9d0f075062a10dfb61b1a69
Taylor Martin <taylormartin@users.noreply.github.com>
2024-05-01 10:30:56 -0400

    looking at other solution

--- sliding-window/max-consecutive-ones.js
+++ sliding-window/max-consecutive-ones.js
@@ -1,26 +1,24 @@
 const testCases = [
-    { nums: [1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0], k: 2, output: 6 },
-    { nums: [0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1], k: 3, output: 10 },
+    // { nums: [1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0], k: 2, output: 6 },
+    // { nums: [0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1], k: 3, output: 10 },
+    // { nums: [0], k: 1, output: 1 },
+    { nums: [0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0], k: 2, output: 6 },
+         //  0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14,15,16,17,18
 ];
 
 function longestOnes(nums, k) {
     if (!nums.includes(0)) return nums.length;
-    if (!nums.includes(1)) return 0;
 
-    // build first window
     let largestWindow = 0;
     let slots = 0;
     let nextWindowEnd = 0;
     for (i = 0; i < nums.length; i++) {
         if (nums[i] === 1) {
-            //found a one
             largestWindow++;
         } else if (slots < k) {
-            //found a zero
             slots++;
             largestWindow++;
         } else {
-            //found a zero and out of slots
             nextWindowEnd = i;
             break;
         }
@@ -33,8 +31,6 @@ function longestOnes(nums, k) {
         if (nums[nextWindowEnd] === 0) slots++;
 
         if (slots < k) {
-            // go down the list filling up slots with the zeros until k is equaled
-            // add 1 to larget window each time
             while (slots < k && nextWindowEnd < nums.length) {
                 if (nums[nextWindowEnd + 1] === 0) {
                     slots++;
@@ -43,23 +39,30 @@ function longestOnes(nums, k) {
                 largestWindow++;
             }
         } else if (slots === k) {
-            //move next window end, see if it's a 1 and repeat until hitting a zero
-            // add 1 to largest window each time
-            // then set largestWindow and shift the window
             while (nums[nextWindowEnd + 1] !== 0 && nextWindowEnd < nums.length - 1) {
                 nextWindowEnd++;
                 largestWindow++;
             }
         }
-        // slots are >= k
-        // shift the window and go to next execution of while loop
         nextWindowStart++;
         nextWindowEnd++;
     }
     return largestWindow;
 };
 
+function longestOnesv2(nums, k) {
+    let pivot = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === 0) k--;
+        if (k < 0) {
+            if (nums[pivot] === 0) k++;
+            pivot++;
+        }
+    }
+    return nums.length - pivot;
+}
+
 testCases.forEach(testCase => {
-    const result = longestOnes(testCase.nums, testCase.k);
+    const result = longestOnesv2(testCase.nums, testCase.k);
     console.log(`${result === testCase.output ? 'PASS' : 'FAIL'} | expected: ${testCase.output} | received: ${result}`);
 });
