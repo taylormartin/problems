0bc9d2bf4af8a072c7799d935272e06340d2f80f
Taylor Martin <taylormartin@users.noreply.github.com>
2024-06-05 16:27:51 -0400

    solved

--- hash-map-set/equal-row-column.js
+++ hash-map-set/equal-row-column.js
@@ -1,5 +1,5 @@
 const testCases = [
-    // { input: [[3, 2, 1], [1, 7, 6], [2, 7, 7]], output: 1 },
+    { input: [[3, 2, 1], [1, 7, 6], [2, 7, 7]], output: 1 },
     { input: [[3, 1, 2, 2], [1, 4, 4, 5], [2, 4, 2, 2], [2, 4, 2, 2]], output: 3 }
 ];
 
@@ -7,23 +7,20 @@ var equalPairs = function (grid) {
     let sideLength = grid.length;
     let equalPairs = 0;
 
-    let rowStart = 0;
-    for (let i = 0; i < sideLength; i++) {
-        let columnStart = 0;
-
-        for (let i = 0; i < sideLength; i++) {
-
-            if (grid[rowStart][i] !== grid[i][columnStart]) {
-                columnStart++;
-                break;
+    for (let row = 0; row < sideLength; row++) {
+        // console.log('next row');
+        for (let column = 0; column < sideLength; column++) {
+            // console.log('next column');
+            for (let i = 0; i < sideLength; i++) {
+                // console.log(`row ${grid[row][i]} col ${grid[i][column]}`)
+                if (grid[row][i] !== grid[i][column]) {
+                    break;
+                } else if (i === sideLength - 1) {
+                    // console.log('equal');
+                    equalPairs++;
+                }
             }
-
-            equalPairs++;
-
         }
-
-
-        rowStart++;
     }
 
     return equalPairs;
