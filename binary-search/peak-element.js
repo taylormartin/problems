573436f7899ce97a2d7d7756f19c8a65c1ef820d
Taylor Martin <taylormartin@users.noreply.github.com>
2024-06-14 15:42:01 -0400

    peak-element solution removed

--- binary-search/peak-element.js
+++ binary-search/peak-element.js
@@ -4,14 +4,7 @@ const testCases = [
 ];
 
 function findPeakElement(nums) {
-    let left = 0, right = nums.length-1, mid;
-    
-    while(left < right) {
-        mid = Math.floor((right+left)/2);
-        if(nums[mid] > nums[mid+1]) right = mid;
-        else left = mid+1;
-    }
-    return left;
+
 }
 
 testCases.forEach(testCase => {
